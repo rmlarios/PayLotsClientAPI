4e30ece2dcbff1dae89f3a15ff3edafaa2f4ac96
rmlarios <mlariosc7@hotmail.com>
2020-12-15 23:11:10 -0600

    15122020-2

--- DevExtremeAspNetCoreResponsiveApp/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ DevExtremeAspNetCoreResponsiveApp/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using DevExtremeAspNetCoreResponsiveApp.Proxies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NToastNotify;
 
 namespace DevExtremeAspNetCoreResponsiveApp.Areas.Identity.Pages.Account
 {
     public class ResetPasswordModel : PageModel
     {
+        private readonly IGenericProxy _genericProxy;
+        private readonly IToastNotification _toastNotification;
         [BindProperty]
-        public ResetPasswordRequest Input { get; set; }
-        public ResetPasswordModel()
+        public ResetPasswordRequest Input { get; set; } 
+        public ResetPasswordModel(IGenericProxy genericProxy, IToastNotification toastNotification)
         {
-
+            _genericProxy = genericProxy;
+            _toastNotification = toastNotification;
         }
         public class ResetPasswordRequest
         {
             [Required]
             [EmailAddress]
-            public string Email { get; set; }
+            [Display(Name ="Se envió un código de confirmación a: ")]
+            public string emailreset { get; set; }
             [Required]
             public string Token { get; set; }
             [Required]
@@ -31,11 +37,30 @@ namespace DevExtremeAspNetCoreResponsiveApp.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
         }
 
-        public void OnGet()
+        public void OnGet(string? email)
         {
+            Input = new ResetPasswordRequest();
+            if (email != null)
+                Input.emailreset = email;
         }
 
-        public void OnPost()
-        { }
+        public async Task<IActionResult> OnPost([FromForm] string emailreset)
+        {
+            Input.emailreset = emailreset;
+            var result = await _genericProxy.PostAsync<ResetPasswordRequest>("Account/ResetPassword", Input);
+            if (result.Succeeded)
+            {
+                //return RedirectToPage("./ForgotPasswordConfirmation");                    
+                _toastNotification.AddSuccessToastMessage(result.Message);
+                return new JsonResult(Input);
+            }
+            else
+            {
+                _toastNotification.AddErrorToastMessage(result.Message);
+                return BadRequest();
+                //return RedirectToPage("./ForgotPasswordConfirmation");
+            }
+
+        }
     }
 }
