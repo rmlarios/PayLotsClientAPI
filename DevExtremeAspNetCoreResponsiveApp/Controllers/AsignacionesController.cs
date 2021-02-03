455da769c3d88e25b658045e5c94d8555dadc4bd
Miguel Angel Larios Campos <mlariosc7@hotmail.com>
2021-02-03 16:53:39 -0600

    Activar Asignacion

--- DevExtremeAspNetCoreResponsiveApp/Controllers/AsignacionesController.cs
+++ DevExtremeAspNetCoreResponsiveApp/Controllers/AsignacionesController.cs
@@ -10,38 +10,65 @@ using Microsoft.AspNetCore.Http;
 
 namespace DevExtremeAspNetCoreResponsiveApp.Controllers
 {
-  public class AsignacionesController : GenericController<ViewAsignacionesSaldo>
-  {
-    IToastNotification _toastNotification;
-
-    public AsignacionesController(IGenericProxy genericProxy, IToastNotification toastNotification) : base(genericProxy, toastNotification, "Asignacion/", "GetAsignaciones", "GetbyBenef/")
+    public class AsignacionesController : GenericController<ViewAsignacionesSaldo>
     {
-      _toastNotification = toastNotification;
+        IToastNotification _toastNotification;
+
+        public class ActivarRequest
+        {
+            public string IdAsignacion { get; set; }
+        }
+
+        public AsignacionesController(IGenericProxy genericProxy, IToastNotification toastNotification) : base(genericProxy, toastNotification, "Asignacion/", "GetAsignaciones", "GetbyBenef/")
+        {
+            _toastNotification = toastNotification;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Anular([FromForm] AnularAsignacionRequest values)
+        {
+            string url = Request.GetTypedHeaders().Referer.ToString();
+
+            //JsonConvert.PopulateObject(values, entity);
+            var result = await _genericProxy.PostAsync<AnularAsignacionRequest>("Asignacion/Anular", values);
+            if (result.Succeeded)
+            {
+                _toastNotification.AddSuccessToastMessage(result.Message);
+                //TempData["Msg"] = "Uploaded successfully";        
+                //return new OkObjectResult(result.Message);
+            }
+            else
+            {
+                _toastNotification.AddErrorToastMessage(result.Message);
+                //TempData["Msg"] = "Uploaded error"; 
+                //return BadRequest(result.Message);
+            }
+            return Redirect(url);
+            //return LocalRedirect("~/Asignacion/Listado");
+            //return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Activar([FromForm] string IdAsignacion)
+        {
+            //    Beneficiarios entity = new Beneficiarios();
+            //  JsonConvert.PopulateObject(values, entity);
+            ActivarRequest request = new ActivarRequest();
+            request.IdAsignacion = IdAsignacion;
+            var result = await _genericProxy.PostAsync<ActivarRequest>("Asignacion/Activar", request);
+            if (result.Succeeded)
+            {
+                  _toastNotification.AddSuccessToastMessage(result.Message);
+                return Ok();
+                //return new OkObjectResult(result.Message);
+            }
+            else
+            {
+                _toastNotification.AddErrorToastMessage(result.Message);
+                return BadRequest(error: result.Message);
+            }
+        }
+
+
     }
-
-    [HttpPost]
-    public async Task<IActionResult> Anular([FromForm] AnularAsignacionRequest values)
-    {
-      string url = Request.GetTypedHeaders().Referer.ToString();
-
-      //JsonConvert.PopulateObject(values, entity);
-      var result = await _genericProxy.PostAsync<AnularAsignacionRequest>("Asignacion/Anular", values);
-      if (result.Succeeded)
-      {
-        _toastNotification.AddSuccessToastMessage(result.Message);
-        //TempData["Msg"] = "Uploaded successfully";        
-        //return new OkObjectResult(result.Message);
-      }
-      else
-      {
-        _toastNotification.AddErrorToastMessage(result.Message);
-        //TempData["Msg"] = "Uploaded error"; 
-        //return BadRequest(result.Message);
-      }
-      return Redirect(url);
-      //return LocalRedirect("~/Asignacion/Listado");
-      //return View();
-    }
-
-  }
 }
--- DevExtremeAspNetCoreResponsiveApp/Proxies/GenericProxy.cs
+++ DevExtremeAspNetCoreResponsiveApp/Proxies/GenericProxy.cs
@@ -13,105 +13,107 @@ using System.Net;
 
 namespace DevExtremeAspNetCoreResponsiveApp.Proxies
 {
-  public interface IGenericProxy
-  {
-    Task<Response<string>> DeleteAsync(string requestUrl);
-    Task<Response<T>> GetAsync<T>(string requestUrl) where T : class;
-    Task<Response<T>> PostAsync<T>(string requestUrl, T model) where T : class;
-    Task<Response<T>> PutAsync<T>(string requestUrl, T model) where T : class;
-  }
-  public class GenericProxy : IGenericProxy
-  {
-    private readonly ProxyHttpClient _proxyHttpClient;
-
-    public GenericProxy(ProxyHttpClient proxyHttpClient)
+    public interface IGenericProxy
     {
-      _proxyHttpClient = proxyHttpClient;
+        Task<Response<string>> DeleteAsync(string requestUrl);
+        Task<Response<T>> GetAsync<T>(string requestUrl) where T : class;
+        Task<Response<T>> PostAsync<T>(string requestUrl, T model) where T : class;
+        Task<Response<T>> PutAsync<T>(string requestUrl, T model) where T : class;
     }
-
-    public async Task<Response<T>> GetAsync<T>(string requestUrl) where T : class
+    public class GenericProxy : IGenericProxy
     {
-      var _httpClient = _proxyHttpClient.Get();
-      var response = _httpClient.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead).Result;
-      if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
-      {
-        return new Response<T>(response.StatusCode.ToString(), false);
-      }
-      else
-      {
-        var data = await response.Content.ReadAsStringAsync();
-        var Json = JsonConvert.DeserializeObject<Response<T>>(data);
-        return Json;
-      }
+        private readonly ProxyHttpClient _proxyHttpClient;
 
-    }
-
-    public async Task<Response<T>> PostAsync<T>(string requestUrl, T model) where T : class
-    {
-      var result = "";
-      try
-      {
-        var _httpClient = _proxyHttpClient.Get();
-        var Content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
-        var response = await _httpClient.PostAsync(requestUrl, Content);
-        if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
+        public GenericProxy(ProxyHttpClient proxyHttpClient)
         {
-          return new Response<T>(response.StatusCode.ToString(), false);
-        }
-        else
-        {
-          result = await response.Content.ReadAsStringAsync();
-          var Json = JsonConvert.DeserializeObject<Response<T>>(result);
-          return Json;
-        }
-      }
-      catch (Exception Ex)
-      {
-        return new Response<T>(Ex.Message);
-      }
-    }
-
-    public async Task<Response<T>> PutAsync<T>(string requestUrl, T model) where T : class
-    {
-      try
-      {
-        var _httpClient = _proxyHttpClient.Get();
-        var Content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
-        var response = await _httpClient.PutAsync(requestUrl, Content);
-        if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
-        {
-          return new Response<T>(response.StatusCode.ToString(), false);
-        }
-        else
-        {
-          var result = await response.Content.ReadAsStringAsync();
-          var Json = JsonConvert.DeserializeObject<Response<T>>(result);
-          return Json;
+            _proxyHttpClient = proxyHttpClient;
         }
 
-      }
-      catch (Exception Ex)
-      {
-        return new Response<T>(Ex.Message);
-      }
+        public async Task<Response<T>> GetAsync<T>(string requestUrl) where T : class
+        {
+            var _httpClient = _proxyHttpClient.Get();
+            var response = _httpClient.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead).Result;
+            if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
+            {
+                return new Response<T>(response.StatusCode.ToString(), false);
+            }
+            else
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var Json = JsonConvert.DeserializeObject<Response<T>>(data);
+                return Json;
+            }
+
+        }
+
+        public async Task<Response<T>> PostAsync<T>(string requestUrl, T model) where T : class
+        {
+            var result = "";
+            try
+            {
+                var _httpClient = _proxyHttpClient.Get();
+
+                var Content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync(requestUrl, Content);
+                if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
+                {
+                    return new Response<T>(response.StatusCode.ToString(), false);
+                }
+                else
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                    var Json = JsonConvert.DeserializeObject<Response<T>>(result);
+                    return Json;
+                }
+            }
+            catch (Exception Ex)
+            {
+                return new Response<T>(Ex.Message);
+            }
+        }
+
+        public async Task<Response<T>> PutAsync<T>(string requestUrl, T model) where T : class
+        {
+            try
+            {
+                var _httpClient = _proxyHttpClient.Get();
+                var Content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync(requestUrl, Content);
+                if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
+                {
+                    return new Response<T>(response.StatusCode.ToString(), false);
+                }
+                else
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    var Json = JsonConvert.DeserializeObject<Response<T>>(result);
+                    return Json;
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                return new Response<T>(Ex.Message);
+            }
+        }
+
+        public async Task<Response<string>> DeleteAsync(string requestUrl)
+        {
+            var _httpClient = _proxyHttpClient.Get();
+            var response = await _httpClient.DeleteAsync(requestUrl);
+            if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
+            {
+                return new Response<string>(response.StatusCode.ToString(), false);
+            }
+            else
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var resp = JsonConvert.DeserializeObject<Response<string>>(result);
+                return new Response<string>(resp.Message, resp.Succeeded);
+            }
+        }
+
+
     }
-
-    public async Task<Response<string>> DeleteAsync(string requestUrl)
-    {
-      var _httpClient = _proxyHttpClient.Get();
-      var response = await _httpClient.DeleteAsync(requestUrl);
-      if (response.StatusCode != HttpStatusCode.BadRequest && response.StatusCode != HttpStatusCode.OK)
-      {
-        return new Response<string>(response.StatusCode.ToString(), false);
-      }
-      else
-      {
-        var result = await response.Content.ReadAsStringAsync();
-        var resp = JsonConvert.DeserializeObject<Response<string>>(result);
-        return new Response<string>(resp.Message, resp.Succeeded);
-      }
-    }
-
-
-  }
 }
